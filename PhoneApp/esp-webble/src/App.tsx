c9c15d8f9b2bb05d6e9db2551d80dfa82b5d18ce
Evgeniya Go <evgeniya.a.golubeva.2021@elsys-bg.org>
2025-11-02 11:56:58 +0200

    Fixed all problems with includes in the app
    56 (?) problems fixed.

--- PhoneApp/esp-webble/src/App.tsx
+++ PhoneApp/esp-webble/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import cubeImg from "./assets/react.svg"; // TODO: replace with your cube asset
 
 // --- NUS UUIDs (ESP32 Nordic UART Service) ---
 const NUS_SERVICE = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
